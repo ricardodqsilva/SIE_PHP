3a199b1f96a1c3df16bc49d064082301a9d37f00
Casal <up201806187@edu.fe.up.pt>
2023-01-24 18:44:09 +0000

    correção

--- actions/action_removeProduct_ShoppCart.php
+++ actions/action_removeProduct_ShoppCart.php
@@ -1,12 +1,19 @@
 <?php
-    session_save_path('C:\data');
-    session_start();                            // Session start to get session variables of current version
-    include('../includes/opendb.php');          // Open communication channel between server and database
-    include_once("../database/store.php");      // Include of PHP functions libraries related with STORE page
+    session_start();                                // Session start to get session variables of current version
+    include('../includes/opendb.php');              // Open communication channel between server and database
+    include_once("../database/shoppcart.php");      // Include of PHP functions libraries related with SHOPPING CART page
 
-    $product = $_POST['product_id_remove'];     // Get product id to be removed from the store (historic data relatec with orders may become inconsistent!)
+    $product = $_POST['product_id_remove'];         // Get product id of item to be removed
 
-    removeProduct($product);                    // Remove the specific product from store
-    
-    header('Location: ../pages/store.php');     // Redirect to store
+    $id_products = array_keys($_SESSION['cart']);
+    $new_cart = [];
+    for($i=0;$i<sizeof($id_products);$i++){
+        if($id_products[$i] == $product){
+            continue;
+        }
+        $new_cart[$id_products[$i]] = $_SESSION['cart'][$id_products[$i]];
+    }
+    $_SESSION['cart'] = $new_cart;                  // Update shopping cart without the item removed from the cart
+
+    header('Location: ../pages/shoppcart.php');     // Redirect to shopping cart to continue the shopping procedure
 ?>
